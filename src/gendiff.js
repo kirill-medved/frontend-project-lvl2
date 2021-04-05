d77a63735825eb6dc55466445f97c205ae2093b5
Kirill Medved <medved.kirill@bk.ru>
2021-04-06 00:38:39 +0300

    Update gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -8,9 +8,28 @@ program
   .arguments('<filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format type [stylish]', 'stylish')
-  .action((f1, f2) => {
-    const result = JSON.stringify(findDiff(f1, f2), null, 2);
-    console.log(result.replace(/"/gi, '').replace(/,/gi, ''));
+  .action((path1, path2, { format }) => {
+    const result = findDiff(path1, path2, format);
+
+    switch (format) {
+      case 'stylish':
+        console.log(
+          JSON.stringify(result, null, 2).replace(/"/gi, '').replace(/,/gi, ''),
+        );
+        break;
+
+      case 'plain':
+        console.log(result);
+        break;
+
+      case 'json':
+        console.log(JSON.stringify(result, null, 2));
+        break;
+
+      default:
+        console.log(result);
+        break;
+    }
   });
 
 program.parse();
