7301f410fcfb0f1fbb9441366a5c6b8f62069358
Kirill Medved <medved.kirill@bk.ru>
2021-04-06 00:34:34 +0300

    Update createAST

--- src/parsers/createAST.js
+++ src/parsers/createAST.js
@@ -2,29 +2,29 @@ import _ from 'lodash';
 import astAPI from '../api/createAstAPI.js';
 
 const createAST = (obj1, obj2) => {
-  const astForObj = (obj) => {
+  const astFromObj = (obj) => {
     const ast = _(obj)
       .toPairs()
       .sort((a, b) => (a > b ? 1 : -1))
       .map(([key, value]) => {
         if (_.isObjectLike(value) && !Array.isArray(value)) {
-          return [key, astForObj(value)];
+          return [key, astFromObj(value)];
         }
         return [key, value];
       })
       .value();
     return ast;
   };
-  const oldAst = astForObj(obj1);
-  const newAst = astForObj(obj2);
+  const oldAst = astFromObj(obj1);
+  const newAst = astFromObj(obj2);
 
   const findDiff = (ast1, ast2, path = '') =>
     _([...ast2, ...ast1])
       .sort((a, b) => (a[0] > b[0] ? 1 : -1))
       .groupBy((item) => item[0])
       .map((arr, parent) => {
-        console.log(JSON.stringify(arr, null, 2));
         if (arr.length === 1) {
+          // property include in old or new file
           const value1 = arr[0][1];
 
           if (ast1.some(([, value]) => value === value1)) {
@@ -34,10 +34,9 @@ const createAST = (obj1, obj2) => {
           if (ast2.some(([, value]) => value === value1)) {
             return astAPI('added', { parent, path, value1 });
           }
-          return 'Ooopss!';
         }
-        const value1 = arr[0][1];
-        const value2 = arr[1][1];
+        const value1 = arr[0][1]; // old data
+        const value2 = arr[1][1]; // new data
 
         if (value1 === value2) {
           return astAPI('same', { parent, path, value1 });
