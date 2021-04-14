43ee1165cae31f90b57b0356361d97436b4a4daf
Kirill Medved <medved.kirill@bk.ru>
2021-04-14 20:46:18 +0300

    Fix actionAST

--- src/utils/actionAST.js
+++ src/utils/actionAST.js
@@ -1,5 +1,8 @@
 const actionAST = (type, props) => {
   // this function collects a ready-made array from the received data to add it to the ast tree
+  let messageValue = null;
+  let messageValue1 = null;
+  let messageValue2 = null;
   switch (type) {
     case 'removed':
       return [
@@ -12,31 +15,47 @@ const actionAST = (type, props) => {
       ];
 
     case 'added':
+      if (Array.isArray(props.value1)) {
+        messageValue = '[complex value]';
+      } else if (typeof props.value1 === 'string') {
+        messageValue = `'${props.value1}'`;
+      } else {
+        messageValue = props.value1;
+      }
       return [
         props.parent,
         {
           type,
           message: `Property '${props.path.concat(
             props.parent,
-          )}' was added with value: ${
-            Array.isArray(props.value1) ? '[complex value]' : props.value1
-          }`,
+          )}' was added with value: ${messageValue}`,
           value: props.value1,
         },
       ];
 
     case 'updated':
+      if (Array.isArray(props.value1)) {
+        messageValue1 = '[complex value]';
+      } else if (typeof props.value1 === 'string') {
+        messageValue1 = `'${props.value1}'`;
+      } else {
+        messageValue1 = props.value1;
+      }
+
+      if (Array.isArray(props.value2)) {
+        messageValue2 = '[complex value]';
+      } else if (typeof props.value2 === 'string') {
+        messageValue2 = `'${props.value2}'`;
+      } else {
+        messageValue2 = props.value2;
+      }
       return [
         props.parent,
         {
           type,
           message: `Property '${props.path.concat(
             props.parent,
-          )}' was updated. From ${
-            Array.isArray(props.value1) ? '[complex value]' : props.value1
-          } to ${
-            Array.isArray(props.value2) ? '[complex value]' : props.value2
-          }`,
+          )}' was updated. From ${messageValue1} to ${messageValue2}`,
           value1: props.value1,
           value2: props.value2,
         },
