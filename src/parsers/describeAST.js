261baee86b8fc9f7b3bba35ec95b8e373c23c397
Kirill Medved <medved.kirill@bk.ru>
2021-04-15 23:43:30 +0300

    Fix 8

--- src/parsers/describeAST.js
+++ src/parsers/describeAST.js
@@ -3,60 +3,60 @@ import actionAST from '../utils/actionAST.js';
 
 const describeAST = (obj1, obj2) => {
   const prepareData = (obj) => _(obj)
-      .toPairs()
-      .sortBy([(o) => o[0]])
-      .map(([key, value]) => {
-        if (_.isObjectLike(value) && !Array.isArray(value)) {
-          return [key, prepareData(value)];
-        }
-        return [key, value];
-      })
-      .value();
+    .toPairs()
+    .sortBy([(o) => o[0]])
+    .map(([key, value]) => {
+      if (_.isObjectLike(value) && !Array.isArray(value)) {
+        return [key, prepareData(value)];
+      }
+      return [key, value];
+    })
+    .value();
 
   const oldAst = prepareData(obj1);
   const newAst = prepareData(obj2);
 
   const findDiff = (ast1, ast2, path = '') => _([...ast1, ...ast2])
-      .sortBy([(o) => o[0]])
-      .groupBy((item) => item[0])
-      .map((arr, parent) => {
-        if (arr.length === 1) {
-          // property include in old or new file
-          const key1 = arr[0][0];
-          const value1 = arr[0][1];
+    .sortBy([(o) => o[0]])
+    .groupBy((item) => item[0])
+    .map((arr, parent) => {
+      if (arr.length === 1) {
+        // property include in old or new file
+        const key1 = arr[0][0];
+        const value1 = arr[0][1];
 
-          if (ast1.some(([key, value]) => value === value1 && key === key1)) {
-            return actionAST('removed', { parent, path, value1 });
-          }
-
-          if (ast2.some(([key, value]) => value === value1 && key === key1)) {
-            return actionAST('added', { parent, path, value1 });
-          }
-
-          return 'something missing';
+        if (ast1.some(([key, value]) => value === value1 && key === key1)) {
+          return actionAST('removed', { parent, path, value1 });
         }
 
-        const value1 = arr[0][1]; // old data
-        const value2 = arr[1][1]; // new data
-
-        if (value1 === value2) {
-          return actionAST('same', { parent, path, value1 });
-        }
-        if (Array.isArray(value1) && Array.isArray(value2)) {
-          return [parent, findDiff(value1, value2, path.concat(`${parent}.`))];
-        }
-        if (value1 !== value2) {
-          return actionAST('updated', {
-            parent,
-            path,
-            value1,
-            value2,
-          });
+        if (ast2.some(([key, value]) => value === value1 && key === key1)) {
+          return actionAST('added', { parent, path, value1 });
         }
 
-        return 'missing something';
-      })
-      .value();
+        return 'something missing';
+      }
+
+      const value1 = arr[0][1]; // old data
+      const value2 = arr[1][1]; // new data
+
+      if (value1 === value2) {
+        return actionAST('same', { parent, path, value1 });
+      }
+      if (Array.isArray(value1) && Array.isArray(value2)) {
+        return [parent, findDiff(value1, value2, path.concat(`${parent}.`))];
+      }
+      if (value1 !== value2) {
+        return actionAST('updated', {
+          parent,
+          path,
+          value1,
+          value2,
+        });
+      }
+
+      return 'missing something';
+    })
+    .value();
 
   const diff = findDiff(oldAst, newAst);
   return diff;
